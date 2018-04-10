9239abfed1b28dac70fcc75d3a50b230ef9c6805
Mikedev <mik3dev@gmail.com>
2018-04-10 23:40:40 +0200

    Fix name

---
+++ constants/__init__.py
@@ -0,0 +1,5 @@
+import dataset
+import model
+import paths
+import procs
+__all__ = ['dataset', 'paths', 'procs', 'model']
---
+++ constants/paths.py
@@ -0,0 +1,11 @@
+from path import Path
+
+PRJ_ROOT = Path(__file__).parent().parent()
+SRC = PRJ_ROOT / 'src'
+DATA = PRJ_ROOT / 'data'
+DATA_RAW = DATA / 'raw'
+DATA_PROCESSING = DATA / 'processing'
+DATA_PROCESSED = DATA / 'processed'
+MODELS = PRJ_ROOT / 'models'
+NOTEBOOKS = PRJ_ROOT / 'notebooks'
+REPORT = PRJ_ROOT / 'report'
