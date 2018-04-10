7695d01a0e3886066bb7fdaca4d56cb5ab55f3a0
Mikedev <mik3dev@gmail.com>
2018-04-10 23:35:41 +0200

    First commit

---
+++ run.py
@@ -0,0 +1,31 @@
+import sys
+from path import Path
+import importlib
+import pkg_resources
+
+PROJECT_ROOT = Path(__file__).parent
+SRC: Path = PROJECT_ROOT / 'src'
+
+
+def update_pythonpath():
+    sys.path += list(SRC.walkdirs())
+    sys.path.append(PROJECT_ROOT / 'costants')
+
+
+def check_requirements():
+    with open(PROJECT_ROOT / 'requirements.txt') as f:
+        pkg_resources.require(f.read().splitlines())
+
+
+def run_module():
+    try:
+        cmd = sys.argv[1]
+        module = importlib.import_module(cmd)
+        module.main()
+    except:
+        raise AttributeError("You must specify the input argument")
+
+
+update_pythonpath()
+check_requirements()
+run_module()
